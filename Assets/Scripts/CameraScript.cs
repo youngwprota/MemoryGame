f160bc8a188390f2e2c033844273d74ec3886a8f
Rusak Pavel <pavelrusak2004@gmail.com>
2024-09-30 08:05:49 +0300

    resolution fix

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraResolutionHandler : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        AdjustCameraSize();
+    }
+
+    void AdjustCameraSize()
+    {
+        float targetAspect = 1600.0f / 1200.0f;  // Целевое соотношение сторон
+        float windowAspect = (float)Screen.width / (float)Screen.height;  // Текущее соотношение сторон экрана
+        float scaleHeight = windowAspect / targetAspect;
+
+        if (scaleHeight < 1.0f)
+        {
+            // Если экран уже по вертикали, увеличиваем размер камеры
+            mainCamera.orthographicSize = mainCamera.orthographicSize / scaleHeight;
+        }
+    }
+}
